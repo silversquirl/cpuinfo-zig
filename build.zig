219b65fa8139d3f21fa016fcdc8b7e1a9d2bdaff
Silver <silver@silv.re>
2023-02-16 14:56:17 +0000

    feat: add build.zig

---
+++ build.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    b.addModule(.{
+        .name = "cpuinfo",
+        .source_file = .{ .path = "cpuinfo.zig" },
+    });
+
+    const test_step = b.addTest(.{
+        .root_source_file = .{ .path = "cpuinfo.zig" },
+    });
+    if (@import("builtin").os.tag == .windows) {
+        test_step.linkLibC();
+    }
+    b.step("test", "Run library tests").dependOn(&test_step.step);
+}
