d633e4a4a8e38bcdbf9cbd3091ac6f2b1c0721d4
mlugg <mlugg@mlugg.co.uk>
2024-07-19 16:33:24 +0100

    Update to latest Zig
    Tested on Zig 0.14.0-dev.367+a57479afc.

--- build.zig
+++ build.zig
@@ -2,14 +2,14 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("cpuinfo", .{
-        .source_file = .{ .path = "cpuinfo.zig" },
+        .root_source_file = b.path("cpuinfo.zig"),
     });
 
     const test_step = b.addTest(.{
-        .root_source_file = .{ .path = "cpuinfo.zig" },
+        .root_source_file = b.path("cpuinfo.zig"),
     });
     if (@import("builtin").os.tag == .windows) {
         test_step.linkLibC();
     }
-    b.step("test", "Run library tests").dependOn(&test_step.step);
+    b.step("test", "Run library tests").dependOn(&b.addRunArtifact(test_step).step);
 }
